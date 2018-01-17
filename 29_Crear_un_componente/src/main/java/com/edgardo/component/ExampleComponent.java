bb7e5637ff8021f26ad005927e5306cb3439b0cf
edgardo vasquez <edgardo.a.v@gmail.com>
2018-01-17 12:36:26 -0300

    Crear un componente en spring

---
+++ 29_Crear_un_componente/src/main/java/com/edgardo/component/ExampleComponent.java
@@ -0,0 +1,15 @@
+package com.edgardo.component;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+//Cuando spring parta, almacenara este componente en "memoria"
+@Component("exampleComponent")
+public class ExampleComponent {
+	private static final Log LOG = LogFactory.getLog(ExampleComponent.class);
+
+	public void sayhello() {
+		LOG.info("HELLO FROM EXAMPLECOMPONENT");
+	}
+}
