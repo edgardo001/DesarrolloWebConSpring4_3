06c410a46ad51a47790b399fc839d2b05e9928de
edgardo vasquez <edgardo.a.v@gmail.com>
2018-01-17 11:11:51 -0300

    Dos formas para redireccionar a otra URL

---
+++ 25_dos_formas_para_redireccionar/src/main/java/com/edgardo/controller/Example3Controller.java
@@ -0,0 +1,51 @@
+package com.edgardo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.edgardo.model.Person;
+
+@Controller
+@RequestMapping("/example3")
+public class Example3Controller {
+
+	private static final String FORM_VIEW = "form";
+	private static final String RESULT_VIEW = "result";
+
+	// Primera Forma para redireccionar
+	/*@GetMapping("/")
+	public String redirect() {
+		return "redirect:/example3/showform";
+	}*/
+
+	// Segunda Forma para redireccionar
+	@GetMapping("/")
+	public RedirectView redirect() {
+		//Indicar el PATH a redireccionar
+		return new RedirectView("/example3/showform");
+	}
+
+	// Muestra el formulario, entregando un objeto de tipo person
+	@GetMapping("/showform")
+	public String showForm(Model model) {
+		model.addAttribute("person", new Person());
+		return FORM_VIEW;
+	}
+
+	// Rescata lo entregado por el formulario y lo muestra
+	@PostMapping("/addperson")
+	public ModelAndView addPerson(@ModelAttribute("person") Person person) {
+		ModelAndView mav = new ModelAndView(RESULT_VIEW);
+		mav.addObject("person", person);
+		return mav;
+	}
+
+}
