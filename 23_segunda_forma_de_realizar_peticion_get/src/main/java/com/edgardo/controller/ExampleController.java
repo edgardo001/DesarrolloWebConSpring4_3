95e36e62bf3103a8126a1e73d948a946000d719b
edgardo vasquez <edgardo.a.v@gmail.com>
2018-01-17 10:30:20 -0300

    Segunda forma de realizar peticion get con thymeleaf

---
+++ 23_segunda_forma_de_realizar_peticion_get/src/main/java/com/edgardo/controller/ExampleController.java
@@ -0,0 +1,54 @@
+package com.edgardo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.edgardo.model.Person;
+
+
+//Bean de Spring, Controller hereda de Component
+@Controller
+//Nombre del primer PATH a nivel de clase
+@RequestMapping("/example")
+public class ExampleController {
+	
+	//Como buena practica se recomienda declarar las vistas en una clase especifica
+	public static final String EXAMPLE_VIEW= "example";
+	
+	//Primera forma
+	//@RequestMapping(value="/exampleString", method=RequestMethod.GET)
+	//Anotacion hija de RequestMapping, para ahorrar escritura
+	@GetMapping("/exampleString")
+	public String exampleString(Model model){
+		//Nombre en la plantilla y su objeto asociado.
+		model.addAttribute("people", getPeople());
+		return EXAMPLE_VIEW;//no es necesario añadir el .html
+	}
+	
+	//Segunda forma
+	//@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
+	//Anotacion hija de RequestMapping, para ahorrar escritura
+	@GetMapping("/exampleMAV")
+	public ModelAndView exampleMAV(){
+		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);//Pasar nombre de la plantilla
+		//Nombre en la plantilla y su objeto asociado.
+		mav.addObject("people", getPeople());
+		return mav;//no es necesario añadir el .html
+	}
+	
+	private List<Person> getPeople(){
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Edgaro", 28));
+		people.add(new Person("Alberto", 30));
+		people.add(new Person("Isabel", 26));
+		people.add(new Person("Carolina", 27));
+		return people;
+	}
+}
