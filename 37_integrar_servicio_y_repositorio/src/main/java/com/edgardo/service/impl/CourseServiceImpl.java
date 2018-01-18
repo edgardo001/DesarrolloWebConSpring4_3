077ec42698e3b4b5aac37bc521f02c4c9988e6c3
edgardo vasquez <edgardo.a.v@gmail.com>
2018-01-18 12:00:43 -0300

    Integrar un servicio y un repository

---
+++ 37_integrar_servicio_y_repositorio/src/main/java/com/edgardo/service/impl/CourseServiceImpl.java
@@ -0,0 +1,47 @@
+package com.edgardo.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.edgardo.entity.Course;
+import com.edgardo.repository.CourseJpaRepository;
+import com.edgardo.service.CourseService;
+
+public class CourseServiceImpl implements CourseService{
+
+	//Instancia de courseJpaRepository
+	@Autowired
+	@Qualifier("courseJpaRepository")
+	private CourseJpaRepository courseJpaRepository;
+	
+	@Override
+	public List<Course> listAllCourses() {		
+		return courseJpaRepository.findAll();
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+		return courseJpaRepository.save(course);
+	}
+
+	@Override
+	public int removeCourse(int id) {
+		courseJpaRepository.delete(id);
+		return 0;
+	}
+
+	@Override
+	public Course updateCourse(Course course) {
+		//Es igual al addCourse.
+		//Intentara actualizar el curso, 
+		//si el curso tiene un id asociado, 
+		//ira a la DB y consultara si existe, 
+		//si existe en la db, realizara una sobreescritura(update) 
+		//del curso entregado
+		courseJpaRepository.save(course);
+		return null;
+	}
+
+}
